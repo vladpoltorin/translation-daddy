d8c56a510f3965474d0dc7cefab20cc74681018e
Vlad Poltorin <vlad.poltorin@gmail.com>
2019-08-01 13:36:32 +0300

    added typescript configs and ts dependencies

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const next = require('next');
+import bodyParser from 'body-parser';
+import express from 'express';
+import next from 'next';
 
 const dev = process.env.NODE_DEV !== 'production';
 const PORT = process.env.PORT || 3000;
@@ -14,12 +14,11 @@ nextApp.prepare().then(() => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
-  app.get('*', (req,res) => {
+  app.get('*', (req: express.Request, res: express.Response) => {
     return handle(req,res);
   });
 
-  app.listen(PORT, err => {
-    if (err) throw err;
+  app.listen(PORT, () => {
     console.log(`ready at http://localhost:${PORT}`);
   });
 });
